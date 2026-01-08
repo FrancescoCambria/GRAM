bb2f0b629e4ecd441f1f47cbc56fdd96f3f53fc1
FrInve <francesco1.invernici@mail.polimi.it>
2026-01-08 16:35:35 +0000

    Remove unused files

--- minegraph-frontend/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Replace ${BASE_PATH} in the nginx template and output to the real config
-envsubst '${BASE_PATH}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-# Execute the CMD from the Dockerfile
-exec "$@"
